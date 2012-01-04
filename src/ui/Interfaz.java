194598f0c40424cdd27ac0b7a7b2ba4cd4d52679
joherma1 <joherma1@bae63100-99b3-3f2c-d208-13b1075658fb>
2012-01-04 16:20:06 +0000

    Limpieza de ficheros y cierre de la migración a Google API v3

--- src/ui/Interfaz.java
+++ src/ui/Interfaz.java
@@ -8,7 +8,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
